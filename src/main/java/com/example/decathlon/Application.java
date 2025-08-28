db7f3a38d67cfe0a3b032808ba09f3eef9c1f6ed
Natte <nattekarlsson4@gmail.com>
2025-08-28 15:01:48 +0200

    Rättning av staffan i maven det ska vara JDK23

--- src/main/java/com/example/decathlon/Application.java
+++ src/main/java/com/example/decathlon/Application.java
@@ -3,7 +3,6 @@ package com.example.decathlon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
